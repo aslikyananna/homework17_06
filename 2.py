938c3f55378d0f569136fb6442791b14f224fa6c
aslikyananna <135046932+aslikyananna@users.noreply.github.com>
2023-06-29 20:15:10 +0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,19 +5,20 @@ while True:
                    "Search for a contact press 2 \n"
                    "List all contacts press 3 \n"
                    "Exit 4 ")
-    if choice == 1:
+    if choice == "1":
         name = input("Add the name: ")
         phone_num = int(input("add phone numb" ))
         L[name] = phone_num
-    elif choice == 2:
+    elif choice == "2":
         search_name = input("writhe the name ")
         if search_name in L.keys():
             print(L[search_name])
         else:
             print("the number do not found ")
-    elif choice == 3:
+    elif choice == "3":
         print(L)
-    elif choice == 4:
+    elif choice == "4":
         print("end")
+        break
     else:
         print("write a correct symbol")
