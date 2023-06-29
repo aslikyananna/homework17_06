0dfe12922c8cc281825ab65bdcb7f9d6500370b8
aslikyananna <135046932+aslikyananna@users.noreply.github.com>
2023-06-29 20:04:31 +0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,18 +1,23 @@
-name = input("write the name ")
-tel_num = input("write the telephone number ")
-names_list = list()
-numbers_list = list()
-names_list.append(name)
-numbers_list.append(tel_num)
-D = dict()
-D = dict(zip(names_list, numbers_list))
-search_name = input("writhe the name ")
-if search_name in names_list:
-    print(D[name])
-else:
-    print("the number do not found ")
-print(D)
-print("end of program ")
-
-
-
+L = {}
+while True:
+    choice = input("if you want \n"
+                   "Add a new contact press 1 \n"
+                   "Search for a contact press 2 \n"
+                   "List all contacts press 3 \n"
+                   "Exit 4 ")
+    if choice == 1:
+        name = input("Add the name: ")
+        phone_num = int(input("add phone numb" ))
+        L[name] = phone_num
+    elif choice == 2:
+        search_name = input("writhe the name ")
+        if search_name in L.keys():
+            print(L[search_name])
+        else:
+            print("the number do not found ")
+    elif choice == 3:
+        print(L)
+    elif choice == 4:
+        print("end")
+    else:
+        print("write a correct symbol")
